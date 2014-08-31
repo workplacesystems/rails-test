a657a6bb4882a68ea3ba383a8a68598493a3a904
Gary Taylor <gary.taylor@hismessages.com>
2014-08-31 21:22:02 +0100

    Added documentation to key areas

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,16 +1,26 @@
+# The controller for the sales resource
 class SalesController < ApplicationController
   include Sales::ControllerConcern
   respond_to :json
   skip_before_filter :verify_authenticity_token
+  # Maps to /sales.json with method of POST
+  # Accepts :sales as either an array of attributes to build the sale or a single object containing the
+  # attributes for a single sale.
+  # The password used to retrieve the sale should be in the 'password' header.
   def create
     @sales = Sale.create(safe_params[:sales])
     @sales = [@sales] unless @sales.instance_of? Array
     render :show
-
   end
+  # Maps to /sales/:id.json with method of GET
+  # Shows the sale as a JSON object.
+  # Must have the 'password' header set to the password saved when created
   def show
     @sales = [Sale.find_secure(params[:id], digested_password)]
   end
+  # Maps to /sales/:id.json with method of DELETE
+  # Deletes the sale and returns the deleted sale
+  # Must have the 'password' header set to the password saved when created
   def destroy
     @sales = [Sale.find_secure(params[:id], digested_password).destroy]
     render :show
@@ -28,5 +38,4 @@ class SalesController < ApplicationController
     render :error, :status => status
 
   end
-  private
 end
